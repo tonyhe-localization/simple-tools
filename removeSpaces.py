7707c0a9f96db906cd1e0038ed4dae24181d5bb3
ubrokemymirror <utony.htj@gmail.com>
2020-07-25 14:12:30 -0700

    removeSpaces.py
    adding removeSpaces.py to repo

---
+++ removeSpaces.py
@@ -0,0 +1,32 @@
+# deletes and replaces spaces in file names to underscores "_".
+
+import os
+import re
+
+def urlify(fileName): # borrowed from stack overflow
+
+    # Remove all non-word characters (everything except numbers and letters)
+    fileName = re.sub(r"[^\w\s]", '', fileName)
+
+    # Replace all runs of whitespace with underscores
+    fileName = re.sub(r"\s+", '_', fileName)
+
+    return fileName
+
+
+path = os.getcwd()
+os.chdir(path)
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+
+    if f_name == 'dateStamp' and f_ext == '.py': # skip the "dateStamp.py" script itself.
+        print('Skipping dateStamp...')
+    elif f_ext == '.ini': # skip any .ini files that you don't want to change the name to.
+        print('Skipping ini file...')
+
+    else:
+        new_name = '{}{}'.format(urlify(f_name), f_ext)
+        os.rename(f, new_name)
+
+
