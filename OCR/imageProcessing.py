1e828f7dedfed028787b1d323642b4ec92c49ab1
ubrokemymirror <utony.htj@gmail.com>
2020-08-06 12:34:00 -0700

    updates to imageProcessing.py

--- OCR/imageProcessing.py
+++ OCR/imageProcessing.py
@@ -1,2 +1,6 @@
 # script that cleans up images (pre-process) before OCR.
 
+import cv2
+import pytesseract
+
+img = cv2.imread(input("Please enter name of the image file you'd like to have pre-processed: "))
