70be88e4723f632dd40b0612fa499a601a84c37d
ubrokemymirror <utony.htj@gmail.com>
2020-08-06 12:37:09 -0700

    updates to imageProcessing.py

--- OCR/imageProcessing.py
+++ OCR/imageProcessing.py
@@ -1,6 +1,10 @@
 # script that cleans up images (pre-process) before OCR.
 
 import cv2
-import pytesseract
+import numpy as np
 
 img = cv2.imread(input("Please enter name of the image file you'd like to have pre-processed: "))
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
