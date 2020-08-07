0b8b689eeca6af4289d651f9d2a18369e012c11a
ubrokemymirror <utony.htj@gmail.com>
2020-08-07 12:50:34 -0700

    updaing imageProcessing.py

--- OCR/imageProcessing.py
+++ OCR/imageProcessing.py
@@ -1,6 +1,6 @@
 # script that cleans up images (pre-process) before OCR.
 
-import cv2
+import cv2 
 import numpy as np
 
 img = cv2.imread(input("Please enter name of the image file you'd like to have pre-processed: "))
@@ -8,3 +8,17 @@ img = cv2.imread(input("Please enter name of the image file you'd like to have p
 # get grayscale image
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
