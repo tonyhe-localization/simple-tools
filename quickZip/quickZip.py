f3e1d1c28b7211cdf2b461efd3593a0338fb6ff9
ubrokemymirror <utony.htj@gmail.com>
2020-08-23 12:49:30 -0700

    writing quickzip

--- quickZip/quickZip.py
+++ quickZip/quickZip.py
@@ -1,11 +1,11 @@
 # when executed, zips each file within a folder individually. Very useful for working on multilingual files and folders.
 
-from zipfile import ZipFile
+from zipfile 
 import os
 
-def filePaths(directory):
-    path = os.getcwd()
-    os.chdir(path)
+path = os.getcwd()
+os.chdir(path)
 
-    for f in os.listdir():
-        
+for f in os.listdir():
+    newZip = zipfile.ZipFile("", "a") # "a" to append, "w" to write.
+    
